5ca796935556a331dbc3cf801fbff15c78345d6f
Margarita Terekhov <margarita@netix.co.il>
2018-07-29 11:08:20 +0300

    Init commit try to push heroku

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static(publicPath));
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log(`Server is up on port ${port}`);
 });
