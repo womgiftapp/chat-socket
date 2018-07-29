17352acd0418e0994cb71fa4de49b108662104db
Margarita Terekhov <margarita@netix.co.il>
2018-07-29 10:01:50 +0300

    Init commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname,'..','public');
+const app = express();
+
+app.use(express.static(publicPath));
+
+const port = 3000;
+app.listen(port,()=>{
+    console.log(`Server is up on port ${port}`);
+});
